43ac49f5c5cc6308a3d1ed55d8bc0f31f9b584e6
root <z.islam370@gmail.com>
2019-05-13 12:42:20 +0600

    add login

---
+++ app/src/main/java/com/rafi/fireinfo/activity/Login.java
@@ -0,0 +1,41 @@
+package com.rafi.fireinfo.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.rafi.fireinfo.R;
+
+public class Login extends AppCompatActivity {
+
+    EditText userEditText, passwordEditText;
+    String user,password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        userEditText = (EditText) findViewById(R.id.editTextLoginUser);
+        passwordEditText = (EditText) findViewById(R.id.editTextLoginPassword);
+
+
+
+    }
+    public void performLogin(View v) {
+        user = userEditText.getText().toString();
+        password = passwordEditText.getText().toString();
+
+        if(user.equals("admin") && password.equals("1234567")){
+
+            Intent checkupHome = new Intent(this,MainActivity.class);
+            startActivity(checkupHome);
+
+        }else {
+
+            Toast.makeText(this, "Invalid User Name or Password", Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
