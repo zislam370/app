92da17826d86e7837b26eec0b13105afed9f5b7a
Rafi <ahmedrafi98@gmail.com>
2019-05-14 11:09:00 +0600

    test commit

--- app/src/main/java/com/rafi/fireinfo/adapter/InfoAdapter.java
+++ app/src/main/java/com/rafi/fireinfo/adapter/InfoAdapter.java
@@ -92,6 +92,7 @@ public class InfoAdapter extends RecyclerView.Adapter<InfoAdapter.ViewHolder>{
 
 
 
+
         holder.imageName.setText(mImageNames.get(position));
         Picasso.get().load(mImages.get(position)).into(holder.image);
         //self
